257350a55c575b4a58464b438698060fc147d052
mkrzytsanek <mkrzystanek@gmail.com>
2020-03-29 13:16:33 +0200

    add problem 2 solution

--- Problem2/FileRecursion.py
+++ Problem2/FileRecursion.py
@@ -1,3 +1,5 @@
+import os
+
 
 def find_files(suffix, path):
     """
@@ -15,4 +17,25 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    files_with_suffix_paths = []
+    check_files(suffix, path, files_with_suffix_paths, "")
+    return files_with_suffix_paths
+
+
+def check_files(suffix, path, result_list, parent_dir):
+    full_directory = os.path.join(parent_dir, path)
+
+    if os.path.isfile(full_directory):
+        if path.endswith(suffix):
+            result_list.append(full_directory)
+        else:
+            return
+    else:
+        subdirectories_list = os.listdir(full_directory)
+        for dir in subdirectories_list:
+            check_files(suffix, dir, result_list, full_directory)
+
+
+directories = find_files(".c", ".")
+print(directories)
+assert directories == ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir1/a.c', './testdir/subdir5/a.c']
