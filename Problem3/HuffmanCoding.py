196bf75029268cc23248bdfcbb00af37aea9a33f
mkrzytsanek <mkrzystanek@gmail.com>
2020-04-04 11:50:53 +0200

    add description to problem 3 solution

--- Problem3/HuffmanCoding.py
+++ Problem3/HuffmanCoding.py
@@ -4,47 +4,21 @@ from HuffmanTree import Tree
 from HuffmanTree import Node
 
 
-def convert_to_nodes_list(letter_frequencies):
-    nodes_list = []
-    for element in letter_frequencies:
-        nodes_list.append(Node(element[0], element[1]))
-    return nodes_list
-
-
-def get_frequency_list(string):
-    chars_set = set([char for char in string])
-    frequencies = set()
-    for char in chars_set:
-        count = 0
-        for letter in string:
-            if char == letter:
-                count += 1
-        frequencies.add((char, count))
-    frequencies = list(frequencies)
-    frequencies.sort(key=operator.itemgetter(1, 0))
-    return frequencies
-
-
 class HuffmanCoding:
-    def huffman_encoding(self, data):
+    def encode(self, data):
         # 1. Determine a frequency of each letter in given string
         frequencies = get_frequency_list(data)
 
         # 2. Create a list with nodes that hold letter and its frequency
         nodes_list = convert_to_nodes_list(frequencies)
 
-        # 3. As long length of list is > 1 merge nodes:
-        #         1. Find two nodes that have the lowest frequency
-        #         2. Create new root node
-        #         3. Root node will store sum of two nodes frequencies and no letter
-        #         4. Two nodes will become root node left and right children
+        # 3. Build Huffman tree from the list of Nodes
         huffman_tree = Tree(self.merge_nodes(nodes_list))
 
-        # 4. Determine binary codes for leaf nodes:
-        #         1. Need to traverse the tree in pre-order fashion
-        #         2. If on the way to reaching leaf algorithm passes left node, 0 is appended to code
-        #         3. If on a way to reaching leaf algorithm passes right node, 1 is appended to code
+        # 4. Determine binary codes for leaf nodes
         binary_codes = self.calculate_binary_codes(huffman_tree)
+
+        # 5. Replace character in string with its code. Because iteration over string is needed, time complexity is O(n)
         encoded_string = ""
         for c in data:
             encoded_string = encoded_string + binary_codes[c]
@@ -54,6 +28,18 @@ class HuffmanCoding:
         return self.get_codes(tree.root, "", {})
 
     def get_codes(self, node, path, codes):
+        """
+        If node has left or right node, it adds respective character ("0" for right anf "1" for left) to the path and
+        calls itself on the child node. If node does not have any children it means it reached a leaf, and tha path is
+        complete. Path is added to the "codes" list. When method reaches the root node again it returns the "codes" list,
+        which contain a path to every leaf in the given tree. Because it is needed to visit every node in the tree,
+        the time complexity of "get_codes" is directly dependent on number of nodes, so time complexity is O(n).
+        :param node: node to check
+        :param path: the path to current node from root, expressed in string of "1" and "0", which mean "go to left node"
+        and "go to right node" respectively
+        :param codes: the list of paths to every tree leaf calculated so far.
+        :return: the list of paths to every tree leaf calculated so far including the path to current node
+        """
         if node.has_left():
             left_path = path + "0"
             self.get_codes(node.left, left_path, codes)
@@ -65,6 +51,18 @@ class HuffmanCoding:
         return codes
 
     def merge_nodes(self, nodes_list):
+        """
+        Method takes the two first elements of the list (which are of the lowest frequency, because list is sorted)
+        and attaches them to a new node, that holds its frequencies sum. The two nodes become new node left and right
+        nodes. The original list is then modified by removing merged two elements and replacing them with new node. New
+        node is attached to list at the right position, depending on its frequency. Method then calls itself on the
+        modified list. When list has only one element, it means we got a tree root.
+        Steps of the method include iterating over the nodes list, which means one method call is O(n) in the worst
+        case. The number of recursive calls is also dependent on the length of the input list. This means the complexity
+        of "merge_nodes: method is O(n^2).
+        :param nodes_list: sorted list of Nodes holding letters and its frequencies
+        :return: root of the Huffman Tree
+        """
         if len(nodes_list) == 1:
             return nodes_list[0]
         new_node = Node()
@@ -82,7 +80,7 @@ class HuffmanCoding:
         new_list.insert(index, new_node)
         return self.merge_nodes(new_list)
 
-    def huffman_decoding(self, data, tree):
+    def decode(self, data, tree):
         """
         Each code is a path to get character of string. 0 means get left child, 1 means get right child. Characters are
         only stored in tree leafs.
@@ -90,10 +88,12 @@ class HuffmanCoding:
         :param tree:
         :return: decodes string
         """
+        # 1. Get a list of decoded characters
         answer = []
         while len(data) != 0:
             data, answer = self.get_string(data, tree.root, answer)
 
+        # 2. Convert list to decoded string, O(n)
         decoded_string = ""
         for c in answer:
             decoded_string = decoded_string + c
@@ -101,6 +101,17 @@ class HuffmanCoding:
         return decoded_string
 
     def get_string(self, data, node, answer):
+        """
+        Method moves across the tree by checking the first character of the encoded data string, moving to the
+        respective child node, and calling itself for the next characters of the data. When it reaches the leaf node,
+        the character value of leaf is appended to the "answer" list. Upon returning to the root node, "answer" list
+        contains all characters in right order. The recursive method is called for every node in the tree, so its
+        time complexity is O(n).
+        :param data: encoded string
+        :param node: node to check
+        :param answer: list of decoded characters fso far
+        :return: tuple of data for current node and answer list
+        """
         if not node.has_right() and not node.has_left():
             answer.append(node.character)
             return data, answer
@@ -109,6 +120,39 @@ class HuffmanCoding:
                 return self.get_string(data[1:], node.left, answer)
             if data[0] == "1" and node.has_right():
                 return self.get_string(data[1:], node.right, answer)
+
+
+def convert_to_nodes_list(letter_frequencies):
+    """
+    A function to convert the tuples of letters and their frequencies to list of Nodes, that will be used to build
+    Huffman Tree. Time complexity is O(n).
+    :param letter_frequencies: set of tuples of letter and its frequency
+    :return: list of Nodes containing letter and its frequency
+    """
+    nodes_list = []
+    for element in letter_frequencies:
+        nodes_list.append(Node(element[0], element[1]))
+    return nodes_list
+
+
+def get_frequency_list(string):
+    """
+    The frequency of each letter it the given string is determined. Because it involves iterating over the string
+    inside for loop of elements of string characters set, the worst case time complexity is O(n^2).
+    :param string: Any string
+    :return: sorted list of tuples of letter and its frequency
+    """
+    chars_set = set([char for char in string])
+    frequencies = set()
+    for char in chars_set:
+        count = 0
+        for letter in string:
+            if char == letter:
+                count += 1
+        frequencies.add((char, count))
+    frequencies = list(frequencies)
+    frequencies.sort(key=operator.itemgetter(1, 0))
+    return frequencies
 
 
 if __name__ == "__main__":
@@ -128,23 +172,21 @@ if __name__ == "__main__":
     tree1 = Tree(merged_tree)
     # tree1.print_tree()
 
-    # test_string = "entire galaxy"
-    # merged_tree2 = merge_nodes(convert_to_nodes_list(get_frequency_list(test_string)))
-    # Tree(merged_tree2).print_tree()
-
     # get binary codes tests
 
     code = huffman_coding.calculate_binary_codes(tree1)
     # print(code)
-    # assert code == ['00', '010', '011', '1']
+    assert code == {'m': '00', 'b': '010', 'l': '011', 'a': '1'}
 
     # encoding test
-    encoded, tree2 = huffman_coding.huffman_encoding(input)
+    encoded, tree2 = huffman_coding.encode(input)
     # print(encoded)
+    assert encoded == "101110101001"
 
     # decoding test
-    decoded_string = huffman_coding.huffman_decoding(encoded, tree2)
+    decoded_string = huffman_coding.decode(encoded, tree2)
     # print(decoded_string)
+    assert decoded_string == "alabama"
 
     codes = {}
 
@@ -153,12 +195,12 @@ if __name__ == "__main__":
     print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
     print ("The content of the data is: {}\n".format(a_great_sentence))
 
-    encoded_data, tree = huffman_coding.huffman_encoding(a_great_sentence)
+    encoded_data, tree = huffman_coding.encode(a_great_sentence)
 
     print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
     print ("The content of the encoded data is: {}\n".format(encoded_data))
 
-    decoded_data = huffman_coding.huffman_decoding(encoded_data, tree)
+    decoded_data = huffman_coding.decode(encoded_data, tree)
 
     print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
     print ("The content of the encoded data is: {}\n".format(decoded_data))
