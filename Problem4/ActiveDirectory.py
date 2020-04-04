43e3ab4b28bbc1e8988eaffa4999a12e5fc00d2b
mkrzytsanek <mkrzystanek@gmail.com>
2020-04-04 12:09:52 +0200

    add descriptions to solutions 5 and 6

--- Problem4/ActiveDirectory.py
+++ Problem4/ActiveDirectory.py
@@ -1,7 +1,7 @@
 class Group(object):
     def __init__(self, _name):
         self.name = _name
-        # changed the initial implementation on list to set, because time complexity of lookup in
+        # changed the initial implementation of list to set, because time complexity of lookup in
         # set is O(1) against O(n) in list. This will help make the search more efficient.
         self.groups = set()
         self.users = set()
@@ -35,6 +35,16 @@ def is_user_in_group(user, group):
 
 
 def find_in_group(user, group, checked):
+    """
+    Function searches the tree of groups and users recursively. Returns True if user is in group, if not, calls itself
+    with every sub group. Because "users" and "groups" of Group object are sets, using "in" operator has only O(1)
+    complexity. However, because in this algorithm groups are visited one by one, the overall complexity depends on the
+    total number of groups in the tree, which is O(n).
+    :param user: user to find
+    :param group: group to search
+    :param checked: a list of groups that were already checked
+    :return: True if user is a part of the group
+    """
     if group.get_name() in checked:
         return
     if user in group.get_users():
--- Problem5/Blockchain.py
+++ Problem5/Blockchain.py
@@ -1,6 +1,9 @@
 import hashlib
 import datetime
 
+# Because I chose to implement BlockChain as LinkedList, operations like "add" will take O(1), while operations that
+# need traversing over chain (like "print_chain") will take O(n).
+
 
 class Block:
     def __init__(self, timestamp, data, previous_hash):
