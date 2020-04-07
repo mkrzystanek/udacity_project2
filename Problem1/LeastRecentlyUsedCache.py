be81228b9f31418136b62cbf71691bf30d734790
mkrzytsanek <mkrzystanek@gmail.com>
2020-04-07 08:49:40 +0200

    fixes after review

--- Problem1/LeastRecentlyUsedCache.py
+++ Problem1/LeastRecentlyUsedCache.py
@@ -104,6 +104,9 @@ test_list.move_to_head(node2)
 print(test_list)  # prints [[9, 10], [11, 12], [7, 8], [1, 2], [5, 6], [3, 4]]
 assert test_list.__repr__() == [[9, 10], [11, 12], [7, 8], [1, 2], [5, 6], [3, 4]]
 
+
+# Cache tests
+
 our_cache = LRU_Cache(5)
 
 our_cache.set(1, 1)
@@ -112,11 +115,27 @@ our_cache.set(3, 3)
 our_cache.set(4, 4)
 
 
-print(our_cache.get(1))       # returns 1
-print(our_cache.get(2))       # returns 2
-print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+print(our_cache.get(1))
+assert our_cache.get(1) == 1
+print(our_cache.get(2))
+assert our_cache.get(2) == 2
+print(our_cache.get(9))
+assert our_cache.get(9) == -1
 
 our_cache.set(5, 5)
 our_cache.set(6, 6)
 
-print(our_cache.get(3))     # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+print(our_cache.get(3))  # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+assert our_cache.get(3) == -1
+
+our_cache.set(1, 9)
+print(our_cache.get(1))
+assert our_cache.get(1) == 9
+
+our_cache.set("1", "aaa")
+print(our_cache.get("1"))
+assert our_cache.get("1") == "aaa"
+print(our_cache.get(1))
+assert our_cache.get(1) == 9
+
+
